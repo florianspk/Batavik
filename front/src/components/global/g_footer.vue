a489fc3e6a4aa604ca249821748bef07105b8f79
Julien <j.keiff@hotmail.fr>
2021-07-04 13:57:18 +0200

    feat(product-card): Add base layout for landing page with products

---
+++ front/src/components/global/g_footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer"></div>
+</template>
+
+<script>
+export default {
+name: 'v-footer'
+}
+</script>
+
+<style scoped lang="scss">
+.footer {
+    position: relative;
+    width: 100vw;
+    height: 120px;
+    margin-top: 2%;
+    background: red;
+}
+</style>
