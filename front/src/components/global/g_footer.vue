3cdb837621d57b8f5a347be4f185022412bf0232
Julien <j.keiff@hotmail.fr>
2021-07-11 21:34:01 +0200

    feat(footer): Create footer

--- front/src/components/global/g_footer.vue
+++ front/src/components/global/g_footer.vue
@@ -1,19 +1,22 @@
 <template>
-  <div class="footer"></div>
+  <div class="footer">
+    
+  </div>
 </template>
 
 <script>
 export default {
-name: 'v-footer'
+  name: 'v-footer'
 }
 </script>
 
 <style scoped lang="scss">
 .footer {
-    position: relative;
-    width: 100vw;
-    height: 120px;
-    margin-top: 2%;
-    background: red;
+  position: relative;
+  width: 100vw;
+  height: 120px;
+  margin-top: 5%;
+  background: white;
+  box-shadow: -5px -5px 10px rgb(0 0 0 / 15%);
 }
 </style>
