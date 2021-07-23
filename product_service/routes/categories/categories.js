e4e931ece4cab447755bb3c981066346506faf14
florianspk <florianspk@gmail.com>
2021-07-23 20:25:39 +0200

    FIX product connection to database

--- product_service/routes/categories/categories.js
+++ product_service/routes/categories/categories.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const connection = require('../connect');
+const connection = require('../../connect');
 
 
 /* GET users listing. */
