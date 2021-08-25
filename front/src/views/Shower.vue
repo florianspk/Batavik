049864464729a73fdd87a6ec3ad9ba012f07f5c3
Julien <j.keiff@hotmail.fr>
2021-08-25 09:17:12 +0200

    feat(item_filter): Add item filter component

--- front/src/views/Shower.vue
+++ front/src/views/Shower.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="shower-page">
+    <item-filter />
     <section-product :nbProduct="6" :type="'bestsellers'" />
   </div>
 </template>
 
 <script>
+import ItemFilter from '../components/item_filter.vue';
 import SectionProduct from '../components/global/g_section.vue';
 
 export default {
   name: 'douche',
-  components: { SectionProduct },
+  components: { ItemFilter, SectionProduct },
 };
 </script>
 
