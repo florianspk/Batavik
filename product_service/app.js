b5de77716fd5172986a66ddf413aaf4a6601a2a6
Florianspk <florianspk@gmail.com>
2021-08-26 09:51:25 +0200

    Revert "New/connection to the database"

--- product_service/app.js
+++ product_service/app.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const productRouter = require('./routes/products/products');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const app = express();
+var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,6 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/products', productRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
