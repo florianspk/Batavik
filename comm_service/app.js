a8a22967c05cb21aea62305c0b0251338c54915a
Soria CAO <Soria.cao@viacesi.fr>
2021-10-12 14:25:19 +0200

    [refactor] : delete useless path,

--- comm_service/app.js
+++ comm_service/app.js
@@ -3,10 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var commentaireRouter = require('./routes/commentaire');
-
 const app = express();
 
 app.use(logger('dev'));
@@ -15,12 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/commentaire', commentaireRouter);
-
 /* routes */
-//app.use('/', require('./routes/comment.js'));
 require('./routes/comment.js')(app)
 
 const db = require("./models");
