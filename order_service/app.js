a282ebc2c2beba52200deadfc52ddeabaed27ea1
Soria CAO <Soria.cao@viacesi.fr>
2021-12-19 19:17:44 +0100

    [ feat ] : order

--- order_service/app.js
+++ order_service/app.js
@@ -3,9 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 app.use(logger('dev'));
@@ -14,11 +11,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
 const db = require("./models");
-db.sequelize.sync({ force: true })
+
+/* routes */
+require('./routes/order.js')(app)
+
+db.sequelize.sync({ force: false })
   .then(() => {
     console.log("Drop and re-sync db.");
   });
