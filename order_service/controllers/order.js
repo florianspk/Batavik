78b3a5c8ef7c2559b061888cfe1a7a252eb504e5
Soria CAO <Soria.cao@viacesi.fr>
2021-12-31 00:02:43 +0100

    [ feat ] : exeption
    exceptions are missing for post order Valid

--- order_service/controllers/order.js
+++ order_service/controllers/order.js
@@ -7,100 +7,124 @@ const order = db.order;
 
 //* Find a single with an id
 exports.findAll = (req, res) => {
-    
-    order.findAll({
-        include:[{
-            model: productCart,
-            include: [{
-                model: cart ,
-                attributes: [],
-                where: {
-                    validation : 1,
-                    idUser : req.body.idUser
-                }
-            }],
-        }]
-      })
+    try{
+        if( typeof req.body.idUser === 'undefined' ){
+          throw new Error("Il manque des informations dans notre requete");
+        }
+          
+        if( isNaN(parseInt(req.body.idUser)) ){
+            throw new Error("Une des valeurs envoyé n'est pas valide");
+        }
+        order.findAll({
+            include:[{
+                model: productCart,
+                include: [{
+                    model: cart ,
+                    attributes: [],
+                    where: {
+                        validation : 1,
+                        idUser : req.body.idUser
+                    }
+                }],
+            }]
+        })
         .then((data) => {
-          res.send(data);
+        res.send(data);
         })
         .catch((err) => {
-          res.status(500).send({
+        res.status(500).send({
             message: err.message || "Some error occurred while retrieving .",
-          });
         });
+        });
+    }catch (error) {
+        res.status(400).send({
+          message: error.message
+        });
+      }
   };
 
 //* Valide order
 exports.validateOrder = (req, res) => {
-    var totalPrices = 0;
-    //listProduct = JSON.parse(req.body.productList);
-    order.create({ 
-        orderToken: "orderToken",
-        orderPrice: 0.00,
-        tradeInformation: "tradeInformation",
-        deadLineOrder: Date.now()
-    }).then(async(order) => {
+    try{
+        if( typeof req.body.idUser === 'undefined' ){
+          throw new Error("Il manque des informations dans notre requete");
+        }
+          
+        if( isNaN(parseInt(req.body.idUser)) ){
+            throw new Error("Une des valeurs envoyé n'est pas valide");
+        }
+        var totalPrices = 0;
+        //listProduct = JSON.parse(req.body.productList);
+        order.create({ 
+            orderToken: "orderToken",
+            orderPrice: 0.00,
+            tradeInformation: "tradeInformation",
+            deadLineOrder: Date.now()
+        }).then(async(order) => {
 
-        const orderPromises = req.body.productList.map(async element => {
-             productcart = await productCart.findOne({
-                include: [{
-                        model: cart ,
-                        attributes: [],
+            const orderPromises = req.body.productList.map(async element => {
+                productcart = await productCart.findOne({
+                    include: [{
+                            model: cart ,
+                            attributes: [],
+                            where: {
+                                validation : 0,
+                                idUser : req.body.idUser
+                            }
+                        }],
+                    where:{
+                        idProduct: element["id"]
+                    },
+                })
+                if(productcart != null){
+                    totalPrices += element["price"] * productcart.quantity;
+
+                    await productcart.update({
+                        unitPrice: element["price"]
+                    })
+                    productcart.save();
+                }else{
+                    await order.destroy();
+                    
+                }
+                
+                
+            })
+
+            Promise.all(orderPromises).then(arrayOfResponses => {
+                order.update({
+                    orderPrice: totalPrices
+                        
+                })
+                console.log("test");
+                order.save().then((order) => 
+                    cart.findOne({
                         where: {
                             validation : 0,
                             idUser : req.body.idUser
                         }
-                    }],
-                where:{
-                    idProduct: element["id"]
-                },
+                    }).then(async(datacart)=> {
+                        if(datacart == null){
+                            res.send({message : 'impossible de trouver votre panier ou votre article ou le client'})
+                        }else{
+                            await datacart.update({validation: 1})
+                            datacart.save();
+                            res.send({message : order})
+                        }
+                        
+                    })
+                );
             })
-            if(productcart != null){
-                totalPrices += element["price"] * productcart.quantity;
 
-                await productcart.update({
-                    unitPrice: element["price"]
-                })
-                productcart.save();
-            }else{
-                await order.destroy();
-                
-            }
-            
             
         })
+        
 
-        Promise.all(orderPromises).then(arrayOfResponses => {
-            order.update({
-                orderPrice: totalPrices
-                    
-            })
-            console.log("test");
-            order.save().then((order) => 
-                cart.findOne({
-                    where: {
-                        validation : 0,
-                        idUser : req.body.idUser
-                    }
-                }).then(async(datacart)=> {
-                    if(datacart == null){
-                        res.send({message : 'impossible de trouver votre panier ou votre article ou le client'})
-                    }else{
-                        await datacart.update({validation: 1})
-                        datacart.save();
-                        res.send({message : order})
-                    }
-                    
-                })
-            );
-        })
-
-         
-    })
-    
-
-    
+    }catch (error) {
+        res.status(400).send({
+        message: error.message
+        });
+    }
     
   };
 
