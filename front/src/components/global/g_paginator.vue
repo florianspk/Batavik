44a9675df52290228f171e86a1704258ad36181d
Julien <j.keiff@hotmail.fr>
2021-12-30 11:33:36 +0100

    feat(api product): Add request to the API product

---
+++ front/src/components/global/g_paginator.vue
@@ -0,0 +1,100 @@
+<template>
+  <div id="paginator">
+    <div id="back-page" class="btn" @click="$emit('previous-page')"> 
+      <img src="@/assets/icons/chevron-back.svg">
+    </div>
+    
+    <div id="page-list">
+      <div class="page" v-for="index in effectivePages" :key="index" :class="index == currentPage ? 'active' : ''">
+        <span v-if="index == 24 && totalPage > 25" @click="showCutomPage">...</span>
+        <span v-else-if="index == 25 && totalPage > 25" @click="$emit('change-page', totalPage)">{{totalPage}}</span>
+        <span v-else @click="$emit('change-page', index)">{{index}}</span>
+      </div>
+    </div>
+
+    <div id="next-page" class="btn" @click="$emit('next-page')"> 
+      <img src="@/assets/icons/chevron-forward.svg">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'paginator',
+  props: ['numberPage', 'currentPage', 'totalPage'],
+  data() {
+    return {
+      effectivePages: 0,
+      showCustomChooser: false,
+    };
+  },
+  methods: {
+    showCutomPage() {
+      this.showCustomChooser = true;
+    },
+    setPageToDisplay() {
+      this.effectivePages = this.totalPage < 25 ? this.totalPage : 25;
+      console.log(this.currentPage);
+    },
+  },
+  watch: {
+    currentPage() {
+      this.setPageToDisplay();
+    },
+  },
+  mounted() {
+    this.setPageToDisplay();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#paginator {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  margin-top: 2vh;
+  width: 85%;
+  height: 8vh;
+  user-select: none;
+  #page-list{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 70%;
+    .page {
+      display: block;
+      background: #fff;
+      margin: 0.5%;
+      padding: 0 1%;
+      border-radius: 2rem;
+      transition-duration: 0.2s;
+      &:hover {
+        background: #ccc;
+        cursor: pointer;
+      }
+    }
+    .active {
+      color: #fff;
+      background-color: #bbb;
+    }
+  }
+  .btn {
+    position: relative;
+    width: 2.5%;
+    background: #fff;
+    border-radius: 2rem;
+    &:hover {
+      background-color: #ccc;
+      cursor: pointer;
+    }
+    img {
+      height: 100%;
+      width: 100%;
+    }
+  }
+}
+</style>
