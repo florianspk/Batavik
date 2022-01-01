df6d7749844680f734e6ac525ef5a67e01b3a534
Julien <j.keiff@hotmail.fr>
2022-01-01 17:53:40 +0100

    feat(luxon): Add luxon

--- front/src/main.ts
+++ front/src/main.ts
@@ -5,6 +5,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { faFacebookSquare, faTwitterSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { DateTime } from 'luxon';
 import Axios from 'axios';
 
 import App from './App.vue';
@@ -37,3 +38,4 @@ app.config.globalProperties.$env = process.env.NODE_ENV;
 app.config.globalProperties.$baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost' : 'https://julienkeiff.fr';
 app.config.globalProperties.$port = Port;
 app.config.globalProperties.$axios = Axios;
+app.config.globalProperties.$date = DateTime;
