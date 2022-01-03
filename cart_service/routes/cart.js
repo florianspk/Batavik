a5dabce49d608b1fcee6f52ad705f82e0adb73f6
Soria CAO <Soria.cao@viacesi.fr>
2022-01-03 20:32:48 +0100

    [ feat ] : comment

--- cart_service/routes/cart.js
+++ cart_service/routes/cart.js
@@ -3,13 +3,46 @@ module.exports = (app) => {
   
     var router = require("express").Router();
   
-    // find one 
+    // find one
+  /**
+   * @openapi
+   * /api/cart:
+   *   get:
+   *     security:
+   *       - jwt: []
+   *     tags:
+   *       - order
+   *     description: recherche le panier non valider d'un utilisateur avec son id (idUser)
+   *     
+  */ 
     router.get("/", cart.findOne);
   
     // Create a new
+    /**
+   * @openapi
+   * /api/cart:
+   *   post:
+   *     security:
+   *       - jwt: []
+   *     tags:
+   *       - order
+   *     description: recherche le panier non valider d'un utilisateur, si il n'existe pas il le creer, en suite il creer un produitCard avec l'id du panier, avec l'id du produit et avec la quantiter
+   *     
+  */ 
     router.post("/", cart.addcart);
   
     // cahnge quentiter or delete a row cart 
+    /**
+   * @openapi
+   * /api/cart/quantityProduct:
+   *   post:
+   *     security:
+   *       - jwt: []
+   *     tags:
+   *       - order
+   *     description: Change la quantiter d'un produit dans produit cart si la quantiter est => 0 on supprime la ligne du produit cart
+   *     
+  */ 
     router.post("/quantityProduct", cart.quantityProduct);
   
     /*
--- order_service/routes/order.js
+++ order_service/routes/order.js
@@ -4,18 +4,58 @@ module.exports = (app) => {
     var router = require("express").Router();
   
     // find one 
+  /**
+   * @openapi
+   * /api/order/all:
+   *   get:
+   *     security:
+   *       - jwt: []
+   *     tags:
+   *       - order
+   *     description: recherche tous les commande d'un utilisteur en fonction de son id (idUser)
+   *     
+  */
     router.get("/all", order.findAll);
   
+  /**
+   * @openapi
+   * /api/order/validate:
+   *   post:
+   *     security:
+   *       - jwt: []
+   *     tags:
+   *       - order
+   *     description: valide le panier, enregistre les prix dans les lignes dans produit cart et creer une linge order et le renseigner dans cart produit
+   *     
+  */
     router.post("/validate", order.validateOrder)
     
+  /**
+   * @openapi
+   * /api/order/cancel/:id:
+   *   post:
+   *     security:
+   *       - jwt: []
+   *     tags:
+   *       - order
+   *     description: change le status d'une commande en "annuler" si elle n'est pas "finish", en fonction de l'id de la commande
+   *     
+  */
     router.post("/cancel/:id", order.cancel)
 
+  /**
+   * @openapi
+   * /api/order/return/:id:
+   *   post:
+   *     security:
+   *       - jwt: []
+   *     tags:
+   *       - order
+   *     description: change le status d'une commande en "return" si elle n'est pas "finish", en fonction de l'id de la commande
+   *     
+  */
     router.post("/return/:id", order.return)
   
-    /*
-    // Retrieve all
-    router.get("/", cart.findOne);
-  */
   
     app.use("/api/order", router);
    
