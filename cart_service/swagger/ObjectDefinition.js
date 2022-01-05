a6f72914377cc14928f9f2d6f266fa18418b6492
Soria CAO <Soria.cao@viacesi.fr>
2022-01-05 22:48:27 +0100

    [ refactor ] : comment of cart service

--- cart_service/swagger/ObjectDefinition.js
+++ cart_service/swagger/ObjectDefinition.js
@@ -19,6 +19,47 @@
  *     type: integer    
  *    idUser:
  *     type: integer
-
+ *  Cart:
+ *   type: object
+ *   properties:
+ *    id: 
+ *     type: integer    
+ *    validation:
+ *     type: integer
+ *    cartPrice:
+ *     type: number
+ *    idUser:
+ *     type: integer
+ *    createdAt:
+ *     type: string
+ *    updatedAt:
+ *     type: string
+ *    productCarts:
+ *     type: array
+ *     items: 
+ *      type: object
+ *      properties:
+ *       id: 
+ *          type: integer
+ *       idProduct: 
+ *          type: integer
+ *       unitPrice: 
+ *          type: integer
+ *       quantity: 
+ *          type: integer
+ *       createdAt: 
+ *          type: string
+ *       updatedAt: 
+ *          type: string
+ *       id_order: 
+ *          type: integer
+ *       id_cart: 
+ *          type: integer
+ * 
+ *  MessageResponse:
+ *   type: object
+ *   properties:
+ *    message: 
+ *     type: string    
  */
 module.exports = {}
