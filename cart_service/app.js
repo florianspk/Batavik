a3acddb1ce85d7fb75ed6d1e3de3d831d561bdbd
Julien <j.keiff@hotmail.fr>
2022-01-08 17:54:43 +0100

    fix(cart_service): Fix cors for cart service server

--- cart_service/app.js
+++ cart_service/app.js
@@ -2,9 +2,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+
+const corsOptions = {
+  origin: process.env.ORIGIN_ALLOWED || "http://localhost:8080",
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 var app = express();
 
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
