642e259217c08fd05aefa1f65f63eac031bf0631
Soria CAO <Soria.cao@viacesi.fr>
2022-01-09 19:39:04 +0100

    [ feat ] : add auth

--- order_service/test/getAllByUser.test.js
+++ order_service/test/getAllByUser.test.js
@@ -1,18 +1,56 @@
 
 const request  = require("supertest");
 const app = require("../app.js");
+const axios = require('axios');
+const qs = require('qs');
 
 describe('searches for the invalid basket of a user with his id', () => {
 
     it('valide test', async() => {
+      //connexion
+      var data = qs.stringify({
+        'email': 'test4',
+        'password': 'test' 
+        });
+        
+        var config = {
+          method: 'post',
+          url: 'http://localhost:3010/api/auth/login',
+          data : data
+        };
+        
+        await axios(config)
+        .then(function (response) {
+          token = response.data.token;
+        })
+        //connexion
       const res = await request(app)
       .get("/api/order/allByUser/1")
+      .set('Authorization', 'Bearer ' + token)
       expect(res.statusCode).toEqual(200);
     })
 
     it('not valide test 1', async() => {
+      //connexion
+      var data = qs.stringify({
+        'email': 'test4',
+        'password': 'test' 
+        });
+        
+        var config = {
+          method: 'post',
+          url: 'http://localhost:3010/api/auth/login',
+          data : data
+        };
+        
+        await axios(config)
+        .then(function (response) {
+          token = response.data.token;
+        })
+        //connexion
         const res = await request(app)
         .get("/api/order/allByUser/string")
+        .set('Authorization', 'Bearer ' + token)
         expect(res.statusCode).toEqual(400);
       })
 
--- order_service/test/postCancel.test.js
+++ order_service/test/postCancel.test.js
@@ -1,21 +1,59 @@
 
 const request  = require("supertest");
 const app = require("../app.js");
+const axios = require('axios');
+const qs = require('qs');
 
 describe('searches for the invalid basket of a user with his id', () => {
 
     it('valide test', async() => {
+      //connexion
+      var data = qs.stringify({
+        'email': 'test4',
+        'password': 'test' 
+        });
+        
+        var config = {
+          method: 'post',
+          url: 'http://localhost:3010/api/auth/login',
+          data : data
+        };
+        
+        await axios(config)
+        .then(function (response) {
+          token = response.data.token;
+        })
+        //connexion
       const res = await request(app)
-      .post("/api/order/cancel/2")
+      .post("/api/order/cancel/1")
+      .set('Authorization', 'Bearer ' + token)
       .send({
-        idUser: 1
+        idUser: 2
       })
       expect(res.statusCode).toEqual(200);
     })
 
     it('not valide test 1', async() => {
+      //connexion
+      var data = qs.stringify({
+        'email': 'test4',
+        'password': 'test' 
+        });
+        
+        var config = {
+          method: 'post',
+          url: 'http://localhost:3010/api/auth/login',
+          data : data
+        };
+        
+        await axios(config)
+        .then(function (response) {
+          token = response.data.token;
+        })
+        //connexion
         const res = await request(app)
         .post("/api/order/cancel/2")
+        .set('Authorization', 'Bearer ' + token)
         .send({
             idUser: 99999
         })
@@ -23,17 +61,53 @@ describe('searches for the invalid basket of a user with his id', () => {
       })
 
     it('not valide test 2', async() => {
+      //connexion
+      var data = qs.stringify({
+        'email': 'test4',
+        'password': 'test' 
+        });
+        
+        var config = {
+          method: 'post',
+          url: 'http://localhost:3010/api/auth/login',
+          data : data
+        };
+        
+        await axios(config)
+        .then(function (response) {
+          token = response.data.token;
+        })
+        //connexion
         const res = await request(app)
         .post("/api/order/cancel/1")
+        .set('Authorization', 'Bearer ' + token)
         .send({
             idUser: "string"
         })
-        expect(res.statusCode).toEqual(204);
+        expect(res.statusCode).toEqual(400);
       })
 
     it('not valide test 2', async() => {
+      //connexion
+      var data = qs.stringify({
+        'email': 'test4',
+        'password': 'test' 
+        });
+        
+        var config = {
+          method: 'post',
+          url: 'http://localhost:3010/api/auth/login',
+          data : data
+        };
+        
+        await axios(config)
+        .then(function (response) {
+          token = response.data.token;
+        })
+        //connexion
         const res = await request(app)
         .post("/api/order/cancel/1")
+        .set('Authorization', 'Bearer ' + token)
         expect(res.statusCode).toEqual(400);
       })
 
