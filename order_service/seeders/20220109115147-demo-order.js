7e740044e4550f4f7f35bb55ff89f09a30507e2b
Julien <j.keiff@hotmail.fr>
2022-01-15 14:01:59 +0100

    fix(seeding): Fix seeding for order db

--- order_service/seeders/20220109115147-demo-order.js
+++ order_service/seeders/20220109115147-demo-order.js
@@ -5,19 +5,22 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     let status_order = ["in progress","finish","return","cancel"];
     let status = [];
-    status_order.forEach(element => {
+    status_order.forEach((element, index) => {
       status.push({
+        id: index+1,
         createdAt: new Date(),
         updatedAt: new Date(),
         status: element
       })
     })
+    await queryInterface.bulkDelete('statuses', {});
     await queryInterface.bulkInsert('statuses',status, {});
 
     let orders = [];
 
     for (let i=0; i < 100; i++){
       orders.push({
+        id: i+1,
         orderToken: casual.sentence,
         orderPrice: casual.double(10, 250),
         tradeInformation: casual.sentence ,
@@ -26,8 +29,8 @@ module.exports = {
         createdAt:new Date(),
         updatedAt:new Date(),
       })
-  }
-
+    }
+    await queryInterface.bulkDelete('orders', {});
     await queryInterface.bulkInsert('orders', orders, {});
 
     let historystatuses = [];
@@ -40,7 +43,7 @@ module.exports = {
         updatedAt:new Date(),
       })
     }
-
+    await queryInterface.bulkDelete('historystatuses', {});
     await queryInterface.bulkInsert('historystatuses', historystatuses, {});
 
     let productCarts = [];
@@ -54,9 +57,25 @@ module.exports = {
         createdAt:new Date(),
         updatedAt:new Date(),
       })
-  }
-
+    }
+    await queryInterface.bulkDelete('productCarts', {});
     await queryInterface.bulkInsert('productCarts', productCarts, {});
+
+    let productOrders = [];
+
+    for (let i=0; i < 300; i++){
+      productOrders.push({
+        idproduct: casual.integer(1, 150),
+        unitPrice: casual.double(10, 100),
+        quantity: casual.integer(1, 5),
+        id_order: casual.integer(1, 100),
+        createdAt:new Date(),
+        updatedAt:new Date(),
+      })
+    }
+
+    await queryInterface.bulkDelete('productOrders', {});
+    await queryInterface.bulkInsert('productOrders', productOrders, {});
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -64,5 +83,6 @@ module.exports = {
     await queryInterface.bulkDelete('orders', null, {});
     await queryInterface.bulkDelete('historystatuses', null, {});
     await queryInterface.bulkDelete('productCarts', null, {});
+    await queryInterface.bulkDelete('productOrders', null, {});
   }
 };
